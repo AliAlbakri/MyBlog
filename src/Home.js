22d602f7de114c3f5653779fc44c67c62abf5313
Ali Albakri <albakri.aliahmed@gmail.com>
2022-01-30 00:36:13 +0300

    styling the page

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
+
+
 const Home = () => {
+    const [name,setName] = useState('maryo');
     return ( 
         <div className="home">
             <h2>Homepage</h2>
